c30730a83b2330e917742c48472d6a88dcf688b2
isildur7 <amey.chaware@gmail.com>
2021-11-01 22:19:05 -0400

    add function to plot history

--- plotting.py
+++ plotting.py
@@ -2,23 +2,26 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 import pandas as pd
 
-def plot_acc(history, ax = None, xlabel = 'Epoch #'):
+def plot_acc(history, losstype, ax = None, xlabel = 'Epoch #'):
     history = history.history
-    history.update({'epoch':list(range(len(history['val_accuracy'])))})
+    val_error_key = "val_"+losstype
+    history.update({'epoch':list(range(len(history[val_error_key])))})
     history = pd.DataFrame.from_dict(history)
 
-    best_epoch = history.sort_values(by = 'val_accuracy', ascending = False).iloc[0]['epoch']
+    best_epoch = history.sort_values(by = val_error_key,
+                                     ascending = True).iloc[0]['epoch']
 
     if not ax:
       f, ax = plt.subplots(1,1)
-    sns.lineplot(x = 'epoch', y = 'val_accuracy', data = history, label = 'Validation', ax = ax)
-    sns.lineplot(x = 'epoch', y = 'accuracy', data = history, label = 'Training', ax = ax)
-    ax.axhline(0.5, linestyle = '--',color='red', label = 'Chance')
-    ax.axvline(x = best_epoch, linestyle = '--', color = 'green', label = 'Best Epoch')  
-    ax.legend(loc = 1)    
-    ax.set_ylim([0.4, 1])
+    sns.lineplot(x = 'epoch', y = val_error_key,
+                 data = history, label = 'Validation', ax = ax)
+    sns.lineplot(x = 'epoch', y = losstype,
+                 data = history, label = 'Training', ax = ax)
+    ax.axvline(x = best_epoch, linestyle = '--',
+               color = 'green', label = 'Best Epoch')  
+    ax.legend(loc = 1)
 
     ax.set_xlabel(xlabel)
-    ax.set_ylabel('Accuracy (Fraction)')
+    ax.set_ylabel(losstype+' (Fraction)')
     
     plt.show()
