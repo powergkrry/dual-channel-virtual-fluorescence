022418a066d584d9dbe8fb0e95b587e66fb854c5
isildur7 <amey.chaware@gmail.com>
2021-11-02 12:52:37 -0400

    remove l1 loss for now

--- main.py
+++ main.py
@@ -75,8 +75,8 @@ def blur_mse_loss(y_true, y_pred):
     
     l2loss = keras.losses.mean_squared_error(blurred_y_true, blurred_y_pred)
     
-    l1loss = tf.keras.regularizers.L1()(y_pred)
-    return l2loss + l1loss
+    # l1loss = tf.keras.regularizers.L1()(y_pred)
+    return l2loss
 
 
 #%%
