896c94c9b05b5a65f40ae53ba7e0c1623b8dc3a8
isildur7 <amey.chaware@gmail.com>
2021-11-03 16:19:45 -0400

    functionify plotting results

--- plotting.py
+++ plotting.py
@@ -25,3 +25,26 @@ def plot_acc(history, losstype, ax = None, xlabel = 'Epoch #'):
     ax.set_ylabel(losstype+' (Fraction)')
     
     plt.show()
+
+
+def plot_predictions(trained_model, testgen):
+  for index in range(8):
+    preds = trained_model.predict(testgen[index][0])
+
+    fig, axs = plt.subplots(nrows=3, ncols=8, figsize=(16, 12))
+    counter = 0
+    for i in range(8):
+        ax = axs[0,counter]
+        ax.axis('off')
+        ax.imshow(testgen[index][0][i,:,:,0], cmap='gray')  # , vmax=0.8
+        ax.set_title(f"Center {counter}")
+        ax = axs[1,counter]
+        ax.axis('off')
+        ax.imshow(testgen[index][1][i,:,:,0], vmin=0.01, vmax=0.4)
+        ax.set_title(f"Ground Truth Green {counter}")
+        ax = axs[2,counter]
+        ax.axis('off')
+        ax.set_title(f"Prediction Green {counter}")
+        ax.imshow(preds[i], vmin=0.01, vmax=0.4)  # , vmin=0.01, vmax=1
+        counter += 1
+    plt.savefig(str(index)+".png")
